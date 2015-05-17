b5280ab9dfa07380c575e63c1cc95c55a2d904bc
Michael DuBois <michael@michaeldubois.me>
2015-05-17 13:26:38 -0700

    * Saved some files with vim configured to remove trailing whitespace.  * Added git completion  * Added more vim swap files to gitignore  * Switching jslinting to eslint

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory 
+# This script creates symlinks from the home directory
 # to any desired dotfiles in ~/dotfiles
 # adapted from: http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
 ############################
@@ -12,11 +12,11 @@
 CWD=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 gitdir=$( cd $DIR;git rev-parse --show-toplevel )
-dir=$gitdir/src                   
+dir=$gitdir/src
 # old dotfiles backup directory
-olddir=$CWD/dotfiles_old             
+olddir=$CWD/dotfiles_old
 # list of files/folders to symlink in homedir
-files="bashrc vimrc vim gitignore gitconfig tmux.conf"    
+files="bashrc vimrc vim gitignore gitconfig tmux.conf git-completion.bash"
 
 ##########
 
@@ -30,11 +30,11 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in cwd to dotfiles_old directory, 
-# then create symlinks 
+# move any existing dotfiles in cwd to dotfiles_old directory,
+# then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv $CWD/.$file $olddir/ 
+    mv $CWD/.$file $olddir/
     echo "Creating symlink to $dir/$file in home directory."
     ln -s $dir/$file $CWD/.$file
 done
